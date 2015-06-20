7ddcaa9b41026ff81c01af0951b5d15cdc57d11f
Vincent FUCHS <vincent.fuchs@gmail.com>
2015-06-20 10:49:07 +0530

    refactored test and added existingfile (forgotten from previous commit)

--- spring-projects/src/test/java/com/github/vincent_fuchs/spring_projects/excelReader/ExcelReaderTest.java
+++ spring-projects/src/test/java/com/github/vincent_fuchs/spring_projects/excelReader/ExcelReaderTest.java
@@ -1,42 +1,39 @@
 package com.github.vincent_fuchs.spring_projects.excelReader;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.io.IOException;
-import java.io.InvalidObjectException;
-import java.net.URL;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 
 public class ExcelReaderTest {
-
+	
+	ExcelReader excelReader;
+	
+	@Before
+	public void initTest(){
+		excelReader=new ExcelReader();
+	}
+	
 	@Test(expected=IllegalStateException.class)
 	public void shouldThrowException_ifNotProperlyConfigured_notExistingInputFile() throws UnexpectedInputException, ParseException, NonTransientResourceException, Exception {
-		
-		ExcelReader excelReader=new ExcelReader();
-		
+			
 		excelReader.init();		
 	}
 	
 	@Test(expected=IOException.class)
 	public void shouldThrowException_ifFileDoesntExist() throws UnexpectedInputException, ParseException, NonTransientResourceException, Exception {
-		
-		ExcelReader excelReader=new ExcelReader();
+				
 		excelReader.setInputFile("nonExistingFile.xlsx");
 		excelReader.init();		
-		
-
 	}
 	
 	
 	@Test
 	public void shouldInitOk_withValidExistingFile() throws UnexpectedInputException, ParseException, NonTransientResourceException, Exception {
-		
-		ExcelReader excelReader=new ExcelReader();
-				
+						
 		excelReader.setInputFile("/com/github/vincent_fuchs/spring_projects/excelReader/simpleInputFile.xlsx");
 		excelReader.init();		
 	
