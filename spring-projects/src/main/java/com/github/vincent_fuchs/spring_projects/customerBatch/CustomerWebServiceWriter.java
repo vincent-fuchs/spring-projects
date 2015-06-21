9c7f3e48f895e224c60ab7ae49b7097fe421f4d0
Vincent FUCHS <vincent.fuchs@gmail.com>
2015-06-21 19:50:44 +0530

    starting to play with webservices but not working for now

--- spring-projects/src/main/java/com/github/vincent_fuchs/spring_projects/customerBatch/CustomerWebServiceWriter.java
+++ spring-projects/src/main/java/com/github/vincent_fuchs/spring_projects/customerBatch/CustomerWebServiceWriter.java
@@ -4,15 +4,22 @@ import java.util.List;
 import org.springframework.batch.item.ItemWriter;
 
 import com.github.vincent_fuchs.spring_projects.customerBatch.domain.Customer;
+import com.github.vincent_fuchs.spring_projects.customerBatch.ws.CustomerWsClient;
 
 
 public class CustomerWebServiceWriter implements ItemWriter<Customer> {
 
+	CustomerWsClient wsClient=new CustomerWsClient();
+	
 	@Override
 	public void write(List<? extends Customer> customersToWrite) throws Exception {
 		
+		
+		
 		for(Customer customer : customersToWrite){
 			System.out.println("RECEIVED CUSTOMER TO WRITE : "+customer.getFirstName());
+			wsClient.sendCustomer(customer);
+			System.out.println("CUSTOMER SENT");
 		}
 	}
 
--- spring-projects/src/test/java/com/github/vincent_fuchs/spring_projects/customerBatch/TestSpecificConfiguration.java
+++ spring-projects/src/test/java/com/github/vincent_fuchs/spring_projects/customerBatch/TestSpecificConfiguration.java
@@ -6,12 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("com/github/vincent_fuchs/spring_projects/customerBatch/customerBatch-test.properties")
+@PropertySource("classpath:com/github/vincent_fuchs/spring_projects/customerBatch/customerBatch-test.properties")
 public class TestSpecificConfiguration {
 
 	@Bean
 	public JobLauncherTestUtils jobLauncherTestUtils(){
 		return new JobLauncherTestUtils();
 	}	
+	
+//	@Bean
+//	public CustomerEndpoint customerEndpoint(){
+//		return new CustomerEndpoint();
+//	}
+	
+	
 	
 }
